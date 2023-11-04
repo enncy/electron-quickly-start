92bafedcb6945ea5105121a15d92fc0c9ed8f348
enncy <877526278@qq.com>
2023-11-04 20:24:32 +0800

    version release 0.0.6

--- scripts/build-app.js
+++ scripts/build-app.js
@@ -1,5 +1,6 @@
 const { series } = require('gulp');
 const { execOut } = require('./utils');
+const fs = require('fs');
 
 function buildWeb() {
 	return execOut('pnpm build', { cwd: '../packages/web' });
@@ -11,7 +12,11 @@ function cleanUp() {
 
 function buildApp() {
 	// mkdir node_modules 确保 node_modules 存在，否则 pnpm dist 会报错
-	return execOut('mkdir -p node_modules && pnpm dist', { cwd: '../packages/app' });
+	const exist = fs.existsSync('../packages/app/node_modules');
+	if (!exist) {
+		fs.mkdirSync('../packages/app/node_modules');
+	}
+	return execOut('pnpm dist', { cwd: '../packages/app' });
 }
 
 exports.default = series(buildWeb, cleanUp, buildApp);
