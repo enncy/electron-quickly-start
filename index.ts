3a3dd0d8d8ede834508e13a2c6b05f82b2f7fa1e
enncy <877526278@qq.com>
2024-01-28 17:47:19 +0800

    add template

---
+++ index.ts
@@ -0,0 +1,70 @@
+#!/usr/bin/env node
+
+import prompts from 'prompts';
+import fs from 'fs';
+import path from 'path';
+
+(async () => {
+	const { project_name } = await prompts({
+		type: 'text',
+		name: 'project_name',
+		message: "What's your project name? (default my-electron-app)",
+		active: 'my-electron-app'
+	});
+	const { project_describe } = await prompts({
+		type: 'text',
+		name: 'project_describe',
+		message: "What's your project describe? (default an electron app)",
+		active: 'an electron app'
+	});
+
+	const sourceDir = path.resolve(__dirname, 'template');
+	const destDir = path.resolve(process.cwd(), project_name);
+
+	copy(sourceDir, destDir);
+
+	const packageJson = JSON.parse(fs.readFileSync(path.resolve(destDir, 'package.json'), 'utf-8'));
+	packageJson.name = project_name;
+	fs.writeFileSync(path.resolve(destDir, 'package.json'), JSON.stringify(packageJson, null, 2));
+
+	const electronBuilderJsonPath = path.resolve(destDir, 'packages', 'app', 'electron.builder.json');
+	const electronBuilderJson = JSON.parse(fs.readFileSync(electronBuilderJsonPath, 'utf-8'));
+	electronBuilderJson.productName = project_name;
+	electronBuilderJson.extraMetadata.name = project_name;
+	electronBuilderJson.extraMetadata.description = project_describe;
+	fs.writeFileSync(electronBuilderJsonPath, JSON.stringify(electronBuilderJson, null, 2));
+
+	const l = console.log;
+
+	l('');
+	l(project_name + ' created!');
+	l('');
+	l('run command below to init project: ');
+	l('');
+	l('	cd ' + project_name);
+	l('	npm install pnpm -g');
+	l('	pnpm install');
+	l('');
+	l('and open tow terminal to run command below: ');
+	l('');
+	l('	npm run dev:web');
+	l('	npm run dev:server');
+})();
+
+function copy(src: string, dest: string) {
+	const stat = fs.statSync(src);
+	if (stat.isDirectory()) {
+		copyDir(src, dest);
+	} else {
+		fs.copyFileSync(src, dest);
+	}
+}
+
+function copyDir(srcDir: string, destDir: string) {
+	fs.mkdirSync(destDir, { recursive: true });
+	for (const file of fs.readdirSync(srcDir)) {
+		const srcFile = path.resolve(srcDir, file);
+		const destFile = path.resolve(destDir, file);
+		copy(srcFile, destFile);
+	}
+}
--- template/readme.md
+++ template/readme.md
@@ -12,13 +12,11 @@ pnpm i
 打开两个终端分别启动 vue 和 electron
 
 ```bash
-cd ./packages/web
-npm run dev
+npm run dev:web
 ```
 
 ```bash
-cd ./packages/app
-npm run dev
+npm run dev:app
 ```
 
 ## 打包
@@ -34,11 +32,8 @@ npm run release
 ```
 
 ## 替换项目占位符以及变量等可修改内容
+ 
 
-全局搜索: `example-name` ， 英文项目名
-
-全局搜索: `https://github.com/enncy/electron-quickly-start` ， 项目 git 地址
-
-替换 packages/app/electron.builder.json 里的 appId 为自己的。
+替换 packages/app/electron.builder.json 里的 appId 为自己的项目域名，例如  electron.baidu.com 。
 
 修改 scripts/release.sh 里的 37 行分支名，如果提交分支是默认的 main 则无需修改。
